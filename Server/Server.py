024bf53a1e1a7f3020c98771955a76a6de974442
Dalton Ng <dustarion@gmail.com>
2019-08-06 19:30:41 +0800

    Made the server continously run to accept new connections from client.
    Basically i added a while loop lol

--- Server/Server.py
+++ Server/Server.py
@@ -20,14 +20,18 @@ PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+
+    ServerShouldRun = True
+
+    while ServerShouldRun: # Run the server continuously
+        conn, addr = s.accept() # Accept Connection from Client
+        with conn:
+            print('Connected by Client', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
 
 
 
